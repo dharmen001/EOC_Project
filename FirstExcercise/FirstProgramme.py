5106a29b6557ff5fb3a009c57a54fa9761959038
dharmendra mishra <dharmendra.mishra@exponential.com>
2017-10-01 22:07:59 +0530

    python project

---
+++ FirstExcercise/FirstProgramme.py
@@ -0,0 +1,9 @@
+intval = 17 % 3
+floatVal = 24.32
+boolVal = True
+print(boolVal)
+boolVal = False
+print intval
+print floatVal
+print boolVal
+
---
+++ FirstExcercise/VariablesAndDataTypes.py
@@ -0,0 +1,6 @@
+VariableNamesCannotBeginWithANumber = True
+boolean = False
+posint = 24
+Zero = 0
+negFloat = -24.35
+VariableNamesCannotBeginWithANumber = False
