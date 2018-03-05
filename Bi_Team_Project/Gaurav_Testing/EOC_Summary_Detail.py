4e261d165861e4d4e15b95f6164e014a16815960
gauravk.singh <gauravk.singh@exponential.com>
2018-03-05 19:00:37 +0530

    EOC_Path Updated on Intraction, Video and Config

--- Bi_Team_Project/Gaurav_Testing/EOC_Summary_Detail.py
+++ Bi_Team_Project/Gaurav_Testing/EOC_Summary_Detail.py
@@ -9,19 +9,28 @@ class Summary_Header():
         self.config=config
 
     def connect_TFR_Summary(self):
-        sql_qry = "select min(SDATE), (CASE WHEN max(EDATE) < trunc(sysdate-1) THEN max(EDATE) ELSE trunc(sysdate-1) END) AS LASTDATE, IO_DESC, CLIENT_DESC, ACCOUNT_MGR, SALES_REP from TFR_REP.SUMMARY_MV where IO_ID = {} group by IO_DESC, CLIENT_DESC, ACCOUNT_MGR, SALES_REP".format(self.config.IO_ID)
-        #sql_qry = "select PLACEMENT_ID, PLACEMENT_DESC, SDATE, EDATE, LDATE, IO_ID, IO_DESC, CLIENT_ID, CLIENT_DESC, BUDGET, COST_PER_UNIT AS UNIT_COST, QUANTITY AS BOOKED_QTY, CREATIVE_DESC, METRIC_DESC, COST_TYPE_DESC, PRODUCT_DIVISION, DATA_SOURCE, ACCOUNT_MGR, SALES_REP from TFR_REP.DAILY_SALES_MV where IO_ID = {} order by PLACEMENT_ID, DAY_DESC".format(self.config.IO_ID)
+        sql_qry = "select PLACEMENT_ID, CREATIVE_DESC, SDATE, LDATE, BUDGET, UNIT_COST, BOOKED_QTY, COST_TYPE_DESC, DATA_SOURCE from TFR_REP.SUMMARY_MV where IO_ID = {} order by PLACEMENT_ID".format(self.config.IO_ID)
         read_sql_Summary=pd.read_sql(sql_qry,self.config.conn)
         return read_sql_Summary
-
 
     def read_query_summary(self):
         read_sql_Summary = self.connect_TFR_Summary()
         return read_sql_Summary
 
+    def read_summary_KM(self):
+        df_KM = self.read_query_summary()
+        df_KM = df_KM[df_KM["DATA_SOURCE"]=='KM']
+        return df_KM
+
+    def read_summary_Sales(self):
+        df_Sales = self.read_query_summary()
+        df_Sales = df_Sales[df_Sales["DATA_SOURCE"]=='SalesFile']
+        return df_Sales
 
     def main(self):
-        df=self.read_query_summary()
+        df=self.read_summary_KM()
+        print(df)
+        df=self.read_summary_Sales()
         print(df)
 
 
